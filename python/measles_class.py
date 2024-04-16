b5daeeb28004f749ccd03b19955834db1cb09362
wagathu <briannjuguna133@gmail.com>
2024-04-16 21:53:01 +0300

    Update on measles class, (instead of inheriting)

--- python/measles_class.py
+++ python/measles_class.py
@@ -11,9 +11,92 @@ import starsim as ss
 import matplotlib.pyplot as plt
 
 # Creating the class measles
-class MyMeasles(ss.Measles):
-    pass
+class Measles(SIR):
 
+    def __init__(self, pars=None, par_dists=None, *args, **kwargs):
+        """ Initialize with parameters """
+
+        pars = ss.omergeleft(pars,
+            # Natural history parameters, all specified in days
+            dur_exp = 8,       # (days) - source: US CDC
+            dur_inf = 11,      # (days) - source: US CDC
+            p_death = 0.005,   # Probability of death
+
+            # Initial conditions and beta
+            init_prev = 0.005,
+            beta = None,
+        )
+
+        par_dists = ss.omergeleft(par_dists,
+            dur_exp   = ss.normal,
+            dur_inf   = ss.normal,
+            init_prev = ss.bernoulli,
+            p_death   = ss.bernoulli,
+        )
+
+        super().__init__(pars=pars, par_dists=par_dists, *args, **kwargs)
+
+        # SIR are added automatically, here we add E
+        self.add_states(
+            ss.State('exposed', bool, False),
+            ss.State('ti_exposed', float, np.nan),
+        )
+
+        return
+
+    @property
+    def infectious(self):
+        return self.infected | self.exposed
+
+    def update_pre(self, sim):
+        # Progress exposed -> infected
+        infected = ss.true(self.exposed & (self.ti_infected <= sim.ti))
+        self.exposed[infected] = False
+        self.infected[infected] = True
+
+        # Progress infected -> recovered
+        recovered = ss.true(self.infected & (self.ti_recovered <= sim.ti))
+        self.infected[recovered] = False
+        self.recovered[recovered] = True
+
+        # Trigger deaths
+        deaths = ss.true(self.ti_dead <= sim.ti)
+        if len(deaths):
+            sim.people.request_death(deaths)
+        return
+
+    def set_prognoses(self, sim, uids, source_uids=None):
+        """ Set prognoses for those who get infected """
+        # Do not call set_prognosis on parent
+        # super().set_prognoses(sim, uids, source_uids)
+
+        self.susceptible[uids] = False
+        self.exposed[uids] = True
+        self.ti_exposed[uids] = sim.ti
+
+        p = self.pars
+
+        # Determine when exposed become infected
+        self.ti_infected[uids] = sim.ti + p.dur_exp.rvs(uids) / sim.dt
+
+        # Sample duration of infection, being careful to only sample from the
+        # distribution once per timestep.
+        dur_inf = p.dur_inf.rvs(uids)
+
+        # Determine who dies and who recovers and when
+        will_die = p.p_death.rvs(uids)
+        dead_uids = uids[will_die]
+        rec_uids = uids[~will_die]
+        self.ti_dead[dead_uids] = self.ti_infected[dead_uids] + dur_inf[will_die] / sim.dt
+        self.ti_recovered[rec_uids] = self.ti_infected[rec_uids] + dur_inf[~will_die] / sim.dt
+
+        return
+
+    def update_death(self, sim, uids):
+        # Reset infected/recovered flags for dead agents
+        for state in ['susceptible', 'exposed', 'infected', 'recovered']:
+            self.statesdict[state][uids] = False
+        return
 
 # Creating the class for the two doses
 class measles_vaccine(ss.sir_vaccine):
@@ -21,7 +104,7 @@ class measles_vaccine(ss.sir_vaccine):
     Create a vaccine product that changes susceptible people to recovered (i.e., perfect immunity)
     """
     def administer(self, people, uids):
-        people.mymeasles.rel_sus[uids] *= 1-self.pars.efficacy
+        people.measles.rel_sus[uids] *= 1-self.pars.efficacy
         return
       
 # Creating the class for the SIAs
@@ -30,7 +113,7 @@ class measles_sia(ss.campaign_vx):
     Create a vaccine product that changes susceptible people to recovered (i.e., perfect immunity)
     """
     def administer(self, people, uids):
-        people.mymeasles.rel_sus[uids] *= 1-self.pars.efficacy
+        people.measles.rel_sus[uids] *= 1-self.pars.efficacy
         return
 
 # The parameters
@@ -43,7 +126,7 @@ pars = sc.objdict(
         contacts = 4
         ),
     diseases = dict(
-        name = 'mymeasles',
+        name = 'measles',
         beta = 0.2
         )
     )
@@ -67,8 +150,8 @@ sim_intv.run()
 
 # Plotting
 plt.figure()
-plt.plot(sim_base.yearvec, sim_base.results.mymeasles.prevalence, label='Baseline')
-plt.plot(sim_intv.yearvec, sim_intv.results.mymeasles.prevalence, label='Vax')
+plt.plot(sim_base.yearvec, sim_base.results.measles.prevalence, label='Baseline')
+plt.plot(sim_intv.yearvec, sim_intv.results.measles.prevalence, label='Vax')
 plt.axvline(x=2013, color='k', ls='--')
 plt.title('Prevalence')
 plt.legend()
